74d0d09e6712e288679ba0e0a3905c9b5ca98d8a
daniel <daniellewis1050@yahoo.com>
2017-01-18 11:56:22 -0600

    Code shift
    Moved code from button method to its own method

--- Form1.cs
+++ Form1.cs
@@ -10,6 +10,8 @@ using System.Windows.Forms;
 
 namespace TimeShift
 {
+    
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -17,29 +19,40 @@ namespace TimeShift
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (this.maskedTextBox1.Text == ("")) 
-            {
-              MessageBox.Show("Please Input a Number", "Enter Number",
-                MessageBoxButtons.OK, MessageBoxIcon.Information);
-            } 
+        #region MagicSauce 
+        public void _magicsauce()
 
-        
+        {
+            if (this.maskedTextBox1.Text == (""))
+            {
+                MessageBox.Show("Please Input a Number", "Enter Number",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
+
             else
-            {    int input;
+            {
+                int input;
                 bool okay = int.TryParse(this.maskedTextBox1.Text, out input);
                 var calc = DateTime.Today.AddMonths(-Math.Abs(input));
                 label1.Text = "" + calc.ToString("MM/yyyy");
-              
+
             }
- 
+        }
+
+        #endregion
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            _magicsauce();
+
+
         }
 
      
 
-       
-private void button2_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
             label1.Text = "";
             maskedTextBox1.Text = "";
